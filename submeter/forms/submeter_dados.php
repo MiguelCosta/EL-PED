47aedad84d03f1ca013875dec699c2a9aab07cb6
Miguel Costa <miguelpintodacosta@gmail.com>
2011-12-18 00:58:16 +0000

    Formulário de Submissão melhorado. Falta agora ajustar a resposta para criar o XML

--- submeter/forms/submeter_dados.php
+++ submeter/forms/submeter_dados.php
@@ -1,4 +1,8 @@
 
+<?php
+include '../ini.php';
+?>
+
 <div id="form_submeter">
     <form name="projetc_record" action="submeter_form_resp.php" method="post" enctype="multipart/form-data">
 
@@ -26,60 +30,137 @@
         <div class="clr"></div>
 
         <div id="form_submeter_supervisor">
-            <b  class="required">Supervisor: </b>
-            <div class="clr"></div>
-            <label for="supervisor1_email"> Email:</label> 
-            <input id="supervisor1_email" name="supervisor1_email" type="text" size="10" maxlength="100" /> 
-
-            <label>Name:</label> 
-            <input name="supervisor1_Name" type="text" size="10" maxlength="100" /> 
-
-            <label>Link:</label> 
-            <input name="supervisor1_link" type="text" size="10" maxlength="100" />
-
-            <label>Department:</label>
-            <input name="supervisor1_Department" type="text" size="10" maxlength="100" />
+            <h3>Supervisors</h3>
             <div class="clr"></div>
 
-            <label>Email:</label> 
-            <input name="supervisor2_email" type="text" size="10" maxlength="100" /> 
-            <label>Name:</label> 
-            <input name="supervisor2_Name" type="text" size="10" maxlength="100" /> 
+            <?php
+            if (!$con) {
+                echo "<h3>Erro ao ligar ao servidor.</h3><br/>" . mysql_error();
+            } else {
+                $sql = "SELECT supcode, name, email,url, affil FROM Supervisor ORDER BY name";
+                $result = mysql_query($sql);
+                $count = mysql_num_rows($result);
+                ?>
 
-            <label>Link:</label> 
-            <input name="supervisor2_link" type="text" size="10" maxlength="100" />
+                <div style="height:100px; overflow: auto; border: 1px solid">
+                    <table class="user">
+                        <tr>
+                            <th class="user">Select</th>
+                            <th class="user">Name</th>
+                            <th class="user">Email</th>
+                            <th class="user">URL</th>
+                            <th class="user">Affil</th>
+                        </tr>
 
-            <label>Department:</label> 
-            <input name="supervisor2_Department" type="text" size="10" maxlength="100" />
+                        <?php
+                        while ($rows = mysql_fetch_array($result)) {
+                            ?>
+                            <tr class="user">
+                                <td class="user"><input name="checkbox[]" type="checkbox" id="checkbox[]" value="<? echo $rows['supcode']; ?>"/></td>
+                                <td class="user"><? echo $rows['name']; ?></td>
+                                <td class="user"><? echo $rows['email']; ?></td>
+                                <td class="user"><? echo $rows['url']; ?></td>
+                                <td class="user"><? echo $rows['affil']; ?></td>
+                            </tr>
+
+                            <?php
+                        }
+                        ?>
+                    </table>
+                <?php } ?>
+            </div>
+
+            <!-- ANTIGO
+                        <label for="supervisor1_email"> Email:</label> 
+                        <input id="supervisor1_email" name="supervisor1_email" type="text" size="10" maxlength="100" /> 
+            
+                        <label>Name:</label> 
+                        <input name="supervisor1_Name" type="text" size="10" maxlength="100" /> 
+            
+                        <label>Link:</label> 
+                        <input name="supervisor1_link" type="text" size="10" maxlength="100" />
+            
+                        <label>Department:</label>
+                        <input name="supervisor1_Department" type="text" size="10" maxlength="100" />
+                        <div class="clr"></div>
+            
+                        <label>Email:</label> 
+                        <input name="supervisor2_email" type="text" size="10" maxlength="100" /> 
+                        <label>Name:</label> 
+                        <input name="supervisor2_Name" type="text" size="10" maxlength="100" /> 
+            
+                        <label>Link:</label> 
+                        <input name="supervisor2_link" type="text" size="10" maxlength="100" />
+            
+                        <label>Department:</label> 
+                        <input name="supervisor2_Department" type="text" size="10" maxlength="100" />
+            -->
         </div>
 
         <div class="clr"></div>
 
         <div id="form_submeter_workteam">
             <h3>WorkTeam</h3>
+            <?php
+            if (!$con) {
+                echo "<h3>Erro ao ligar ao servidor.</h3><br/>" . mysql_error();
+            } else {
+                $sql = "SELECT * FROM Author ORDER BY name";
+                $result = mysql_query($sql);
+                $count = mysql_num_rows($result);
+                ?>
 
-            <label>Email:</label> <input name="workteam1_email" type="text"
-                                         size="25" maxlength="100" /> 
-            <label>Name:</label> <input name="workteam1_Name"
-                                        type="text" size="25" maxlength="100" /> 
-            <label>ID:</label> <input
-                name="workteam1_id" type="text" size="25" maxlength="100" />
+                <div style="height:100px; overflow: auto; border: 1px solid">
+                    <table class="user">
+                        <tr>
+                            <th class="user">Select</th>
+                            <th class="user">Name</th>
+                            <th class="user">ID</th>
+                            <th class="user">EMAIL</th>
+                            <th class="user">URL</th>
+                        </tr>
 
-            <br/>
-            <label>Email:</label> <input name="workteam2_email" type="text"
-                                         size="25" maxlength="100" /> 
-            <label>Name:</label> <input name="workteam2_Name"
-                                        type="text" size="25" maxlength="100" /> 
-            <label>ID:</label> <input
-                name="workteam2_id" type="text" size="25" maxlength="100" />
-            <br />
+                        <?php
+                        while ($rows = mysql_fetch_array($result)) {
+                            ?>
+                            <tr class="user">
+                                <td class="user"><input name="checkbox[]" type="checkbox" id="checkbox[]" value="<? echo $rows['authorcode']; ?>"/></td>
+                                <td class="user"><? echo $rows['name']; ?></td>
+                                <td class="user"><? echo $rows['id']; ?></td>
+                                <td class="user"><? echo $rows['email']; ?></td>
+                                <td class="user"><? echo $rows['url']; ?></td>
+                            </tr>
 
-            <label>Email:</label> <input name="workteam3_email" type="text"
-                                         size="25" maxlength="100" /> 
-            <label>Name:</label> <input name="workteam3_Name"
-                                        type="text" size="25" maxlength="100" /> 
-            <label>ID:</label> <input
-                name="workteam3_id" type="text" size="25" maxlength="100" />
+                            <?php
+                        }
+                        ?>
+                    </table>
+                <?php } ?>
+            </div>
+            <!-- ANTIGO
+                        <label>Email:</label> <input name="workteam1_email" type="text"
+                                                     size="25" maxlength="100" /> 
+                        <label>Name:</label> <input name="workteam1_Name"
+                                                    type="text" size="25" maxlength="100" /> 
+                        <label>ID:</label> <input
+                            name="workteam1_id" type="text" size="25" maxlength="100" />
+            
+                        <br/>
+                        <label>Email:</label> <input name="workteam2_email" type="text"
+                                                     size="25" maxlength="100" /> 
+                        <label>Name:</label> <input name="workteam2_Name"
+                                                    type="text" size="25" maxlength="100" /> 
+                        <label>ID:</label> <input
+                            name="workteam2_id" type="text" size="25" maxlength="100" />
+                        <br />
+            
+                        <label>Email:</label> <input name="workteam3_email" type="text"
+                                                     size="25" maxlength="100" /> 
+                        <label>Name:</label> <input name="workteam3_Name"
+                                                    type="text" size="25" maxlength="100" /> 
+                        <label>ID:</label> <input
+                            name="workteam3_id" type="text" size="25" maxlength="100" />
+            -->
         </div>
 
         <div class="clr"></div>
@@ -93,35 +174,71 @@
             <input type="button" value="paragraph" onclick="insereTextoParagraph()"/>
             <input type="button" value="key word" onclick="insereTextoKW()"/>
             <input type="button" value="url" onclick="insereTextoXRef()"/>
-            <input type="button" value="html" onclick="text2html()"/>
 
         </div>
+
         <div id="form_submeter_deliverables">
             <h3>Deliverables</h3>
-            <label>Name:</label><input type="text" name="deliverable1_name"/>
-            <label>File:</label><input type="file" name="deliverable1_file" id="file1"/>
+
+            <div id="file1" style="display: none">
+                <label id="d_label_n_1">Name:</label>
+                <input type="text" name="deliverable1_name"/>
+                <label id="d_label_f_1">File:</label>
+                <input type="file" name="deliverable1_file" id="file1"/>
+            </div>
             <div class="clr"></div>
-            <label>Name:</label><input type="text" name="deliverable2_name" size="25" maxlength="100"/>
-            <label>File:</label><input type="file" name="deliverable2_file" id="file2"/>
+
+            <div id="file2" style="display: none">
+                <label id="d_label_n_2">Name:</label>
+                <input type="text" name="deliverable2_name"/>
+                <label id="d_label_f_2">File:</label>
+                <input type="file" name="deliverable2_file" id="file2"/>
+            </div>
             <div class="clr"></div>
-            <label>Name:</label><input type="text" name="deliverable3_name" size="25" maxlength="100"/>
-            <label>File:</label><input type="file" name="deliverable3_file" id="file3"/>
+
+            <div id="file3" style="display: none">
+                <label id="d_label_n_3">Name:</label>
+                <input type="text" name="deliverable3_name"/>
+                <label id="d_label_f_3">File:</label>
+                <input type="file" name="deliverable3_file" id="file3"/>
+            </div>
             <div class="clr"></div>
-            <label>Name:</label><input type="text" name="deliverable4_name" size="25" maxlength="100"/>
-            <label>File:</label><input type="file" name="deliverable4_file" id="file3"/>
+
+            <div id="file4" style="display: none">
+                <label id="d_label_n_4">Name:</label>
+                <input type="text" name="deliverable4_name"/>
+                <label id="d_label_f_4">File:</label>
+                <input type="file" name="deliverable4_file" id="file4"/>
+            </div>
             <div class="clr"></div>
-            <label>Name:</label><input type="text" name="deliverable5_name" size="25" maxlength="100"/>
-            <label>File:</label><input type="file" name="deliverable5_file" id="file3"/>
+
+            <div id="file5" style="display: none">
+                <label id="d_label_n_5">Name:</label>
+                <input type="text" name="deliverable5_name"/>
+                <label id="d_label_f_5">File:</label>
+                <input type="file" name="deliverable5_file" id="file5"/>
+            </div>
             <div class="clr"></div>
-            <label>Name:</label><input type="text" name="deliverable6_name" size="25" maxlength="100"/>
-            <label>File:</label><input type="file" name="deliverable6_file" id="file3"/>
+
+            <div id="file6" style="display: none">
+                <label id="d_label_n_6">Name:</label>
+                <input type="text" name="deliverable6_name"/>
+                <label id="d_label_f_6">File:</label>
+                <input type="file" name="deliverable6_file" id="file6"/>
+            </div>
+            <div class="clr"></div>
+
+            <input id="add_del" type="button" name="add_deli" value="add" onclick="addDeliverable()" style="width: 50px; text-align: center"/>
+            <input id="rem_del" type="button" name="rem_deli" value="remove" onclick="removeDeliverable()" style="width: 50px; text-align: center"/>
         </div>
         <hr />
 
-        <input type="submit" value="Enviar" />
+        <div id="btn_user">
+            <input id="submit" type="button" value="Enviar" />
+        </div>
     </form>
 </div>
-
+<div class="clr"></div>
 <span style="float: right" class="required">Campo Obrigatório</span>
 
 
@@ -130,14 +247,36 @@
 
 <script language="javascript">  
     
-    function text2html() {
-        document.project_record.abstract_text.value += "newtext";
-        var newtext = doctype.project_record.abstract_text.value;
-        if (document.project_record.placement[1].checked) {
-            document.project_record.abstract_text.value = "";
+    var files = 0;
+    
+    function addDeliverable(){
+        if(files == 6){
+            alert("Máximo de 6 deliverables!");
         }
         
-        document.project_record.abstract_text.value += "newtext";
+        if(files < 7){
+            if(files < 6 ) files++;
+            var file = 'file'+(files+'');
+            document.getElementById(file).style.display = "inline";
+        } else {
+            
+        }
+        
+        if(files > 6){
+            files = 6;
+        }
+    }
+    
+    function removeDeliverable(){
+        if(files > 0){
+            
+            var file = 'file'+(files+'');
+            document.getElementById(file).style.display = "none";
+            if(files > 1) files--;
+        }
+        if(files < 1){
+            files = 0;
+        }
     }
     
     function insereTextoBold(){

c93e8ad36b6531b0600f7e238796d15a253327e4
Miguel Costa <miguelpintodacosta@gmail.com>
2012-01-21 14:42:23 +0000

    páginas que não tem menu esquerdo alteradas

--- logs/logs.php
+++ logs/logs.php
@@ -17,11 +17,11 @@ if (!isset($_SESSION['username']) || !$_SESSION['username'] || ((isset($_SESSION
             <?php
             include '../header.php';
             include '../menus/menu_logs.php';
-            include '../menus/leftmenuLogs.php';
+            //include '../menus/leftmenuLogs.php';
             ?>
 
 
-            <div id="content">
+            <div id="content_no_left">
                 <div id="content_top"></div>
                 <div id="content_main">
                     <h2>Logs</h2>

16a027223b5ba3414fc4dd92df0ff2a846095228
Miguel Costa <miguelpintodacosta@gmail.com>
2012-01-21 14:06:03 +0000

    caixa de procura alterada para mais bonita :P

--- indexing/search.php
+++ indexing/search.php
@@ -1,11 +1,20 @@
 <?php
-   $dir = "indexing/";
-   if (basename(getcwd()) != 'PED-Project') $dir = "../indexing/";
+$dir = "indexing/";
+$image = "css/images/search2.png";
+if (basename(getcwd()) != 'PED-Project'){
+    $dir = "../indexing/";
+    $image = "../css/images/search2.png";
+}
 ?>
 
-<div id="login">
-   <form method="POST" action="<?= $dir ?>search2.php">
-	  <input type="text" name="query" value="<?php if(isset($_POST['query'])) echo $_POST['query']; ?>"/>
-	  <input type="submit" value="Search" />
-   </form>
+<div id="search">
+    <form method="POST" action="<?= $dir ?>search2.php">
+        <div id="search_form">
+            <div id="search_form_text">
+                <input type="text" placeholder="procurar..." required="" name="query" value="<?php if (isset($_POST['query']))
+    echo $_POST['query']; ?>"/>
+            </div>
+            <input type="image" src="<? echo $image;?>"/>
+        </div>
+    </form>
 </div>
